2a13239541061463edf9ed856baba379aac4fa3d
Donn De Lima <dodelima@my.cspc.edu.ph>
2025-03-25 15:16:19 +0800

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,16 @@ import streamlit as st
 
 st.title("🎈 Welcome to the Black Parade by My Chemical Romance")
 st.write(
-    "When I was a young boy"
-    "My father took me into the city"
-    "To see a marching band"
-    "He said, \"Son, when you grow up"
-    "\Would you be the savior of the broken"
-    "The beaten and the damned?\""
-    "He said, \"Will you defeat them?"
-    "Your demons, and all the non-believers"
-    "The plans that they have made?\""
-    "Because one day, I'll leave you a phantom"
-    "To lead you in the summer"
-    "To join the black parade"
+    "When I was a young boy\n"
+    "My father took me into the city\n"
+    "To see a marching band\n"
+    "He said, \"Son, when you grow up\n"
+    "\Would you be the savior of the broken\n"
+    "The beaten and the damned?\"\n"
+    "He said, \"Will you defeat them?\n"
+    "Your demons, and all the non-believers\n"
+    "The plans that they have made?\"\n"
+    "Because one day, I'll leave you a phantom\n"
+    "To lead you in the summer\n"
+    "To join the black parade\n"
 )
