cbc298a057025860383124d86301dea243936e85
Donn De Lima <dodelima@my.cspc.edu.ph>
2025-03-25 15:11:11 +0800

    commit # 1.3

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 st.title("🎈 Achupelekoy")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "When I was a young boy, my father took me into the city to see the marching band."
+    "He said, \'Son, when you grow up, will you be the savior of the broken, the beaten and the damned?\'"
 )
